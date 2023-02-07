32ecc782229b11ff2450a063145fa4123e5addab
Taranveer Singh <taranveersingh93@gmail.com>
2023-02-07 05:42:57 -0500

    Complete the for loop tasks

--- for.js
+++ for.js
@@ -3,17 +3,29 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
+for(var i=26; i<75; i+=2) {
+  console.log(i)
+}
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
+for(var i = 0; i<weather_options.length; i++) {
+  console.log(`According to the forecasts, it's expected to be ${weather_options[i]} today.`)
+}
 
 // Challenge 3
-// Use a for statement to log to the console only the words that are exactly 3 characters in length
+// Use a for statement to log to the console only the words that are 
+//exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+for (var i=0; i<words.length; i++) {
+  if (words[i].length === 3) [
+    console.log(words[i])
+  ]
+}
+
+
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges. 
   // The recording doesn't have to be the first time you are solving the problem. 
--- selfEvaluation.md
+++ selfEvaluation.md
@@ -1,6 +1,13 @@
 # Self Evaluation
 
 - How confident do you feel in your understanding and fluency with the `for` statement?
+=5/5
+
 - This was a much more advanced concept than anything you've done in Mod 0, and the exercises required you to push yourself to apply other background knowledge or learn things outside of the curriculum - how did that feel? Did you have productive struggle? Unproductive struggle?
+=That felt comfortable. No unproductive struggling. Couple of errors in code syntax but that was instantly fixed.
+
 - What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
+= I feel comfortable. No calendar adjustments needed.
+
 - What questions do you still have? How will you get the answers you need?
+= I'm aware there are more loop methods. Is there a reason why we use "for" loop?
